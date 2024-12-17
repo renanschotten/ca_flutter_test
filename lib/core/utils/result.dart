0ef3a057c147791748e09a86980266e288aa1cd4
Renan Schotten <renan.schotten@gmail.com>
2024-12-17 09:41:43 -0300

    feat: added Equatable to Result class

--- lib/core/utils/result.dart
+++ lib/core/utils/result.dart
@@ -1,4 +1,6 @@
-sealed class Result<T> {
+import 'package:equatable/equatable.dart';
+
+sealed class Result<T> extends Equatable {
   const Result();
   const factory Result.ok(T value) = Ok._;
 
@@ -9,10 +11,16 @@ final class Ok<T> extends Result<T> {
   const Ok._(this.value);
 
   final T value;
+
+  @override
+  List<Object?> get props => [value];
 }
 
 final class Error<T> extends Result<T> {
   const Error._(this.error);
 
   final Exception error;
+
+  @override
+  List<Object?> get props => [error];
 }
