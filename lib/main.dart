33217e537862c26d6118933b31276e284becfd46
Renan Schotten <renan.schotten@gmail.com>
2024-12-16 21:15:29 -0300

    feat: added firebase core and firebase auth dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:ca_flutter_test/blog_module.dart';
 import 'package:ca_flutter_test/blog_widget.dart';
+import 'package:ca_flutter_test/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ModularApp(
       module: BlogModule(),
