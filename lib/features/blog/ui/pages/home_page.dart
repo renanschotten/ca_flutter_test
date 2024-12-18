e00cf10afc197ec8678b01bf2ee7a9458b330e9e
Renan Schotten <renan.schotten@gmail.com>
2024-12-17 21:47:59 -0300

    feat: created HomePage

---
+++ lib/features/blog/ui/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:ca_flutter_test/features/blog/interactor/controller/home_controller.dart';
+
+import 'package:ca_flutter_test/features/blog/interactor/states/home_state.dart';
+import 'package:ca_flutter_test/features/blog/ui/widgets/post_widget.dart';
+import 'package:ca_flutter_test/features/shared/connectivity/ui/widgets/failure_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.controller});
+  final HomeController controller;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.getPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: widget.controller.homeState,
+        builder: (context, state, _) {
+          return switch (state) {
+            LoadingHomeState() => Center(
+                child: CircularProgressIndicator(),
+              ),
+            SuccessHomeState() => ListView.separated(
+                padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 24.0),
+                itemCount: state.posts.length,
+                itemBuilder: (context, index) => PostWidget(
+                  post: state.posts[index],
+                ),
+                separatorBuilder: (context, index) => SizedBox(height: 8.0),
+              ),
+            FailureHomeState() => FailureWidget(
+                title: "Ocorreu um erro ao processar a sua requisição",
+                description: "Tente novamente em alguns instantes",
+                buttonText: "Tentar Novamente",
+                onTapButton: () => widget.controller.getPosts(),
+              ),
+            InitialHomeState() => SizedBox(),
+          };
+        },
+      ),
+    );
+  }
+}
