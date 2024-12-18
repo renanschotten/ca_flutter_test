95321f7f641f990bd7e8c1ac52323e04b38ef6c4
Renan Schotten <renan.schotten@gmail.com>
2024-12-17 21:46:08 -0300

    feat: added route home and new dependencies

--- lib/blog_module.dart
+++ lib/blog_module.dart
@@ -3,6 +3,14 @@ import 'package:ca_flutter_test/features/auth/data/services/firebase_auth_servic
 import 'package:ca_flutter_test/features/auth/interactor/controller/login_controller.dart';
 import 'package:ca_flutter_test/features/auth/interactor/services/i_auth_service.dart';
 import 'package:ca_flutter_test/features/auth/ui/pages/login_page.dart';
+import 'package:ca_flutter_test/features/blog/data/ds/blog_ds.dart';
+import 'package:ca_flutter_test/features/blog/data/repositories/blog_repository.dart';
+import 'package:ca_flutter_test/features/blog/data/services/http_api_service.dart';
+import 'package:ca_flutter_test/features/blog/interactor/controller/home_controller.dart';
+import 'package:ca_flutter_test/features/blog/interactor/datasources/i_blog_ds.dart';
+import 'package:ca_flutter_test/features/blog/interactor/repositories/i_blog_repository.dart';
+import 'package:ca_flutter_test/features/blog/interactor/services/i_api_service.dart';
+import 'package:ca_flutter_test/features/blog/ui/pages/home_page.dart';
 import 'package:ca_flutter_test/features/splash/ui/pages/splash_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -12,11 +20,30 @@ class BlogModule extends Module {
   @override
   void binds(Injector i) {
     i.addSingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+
+    // Services
     i.addSingleton<IAuthService>(
       () => FirebaseAuthService(firebaseAuth: i<FirebaseAuth>()),
     );
+    i.addSingleton<IApiService>(() => HttpApiService());
+
+    // DataSources
+    i.addSingleton<IBlogDataSource>(
+      () => BlogDataSource(service: i<IApiService>()),
+    );
+
+    // Repositories
+    i.addSingleton<IBlogRepository>(
+      () => BlogRepository(dataSource: i<IBlogDataSource>()),
+    );
+
+    // Controllers
     i.addSingleton<LoginController>(
-        () => LoginController(authService: i<IAuthService>()));
+      () => LoginController(authService: i<IAuthService>()),
+    );
+    i.addSingleton<HomeController>(
+      () => HomeController(repository: i<IBlogRepository>()),
+    );
   }
 
   @override
@@ -29,6 +56,12 @@ class BlogModule extends Module {
       Routes.login,
       child: (context) => LoginPage(
         loginController: Modular.get<LoginController>(),
+      ),
+    );
+    r.child(
+      Routes.home,
+      child: (context) => HomePage(
+        controller: Modular.get<HomeController>(),
       ),
     );
   }
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,5 @@
 abstract final class Routes {
   static const splash = '/';
   static const login = '/login';
+  static const home = '/home';
 }
