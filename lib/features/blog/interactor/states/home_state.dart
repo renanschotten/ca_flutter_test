92eacc9bbb5eb17dfa960bfafcb9df817cc49f56
Renan Schotten <renan.schotten@gmail.com>
2024-12-17 21:50:18 -0300

    feat: HomeState

---
+++ lib/features/blog/interactor/states/home_state.dart
@@ -0,0 +1,28 @@
+import 'package:ca_flutter_test/features/blog/interactor/entities/post_entity.dart';
+import 'package:equatable/equatable.dart';
+
+sealed class HomeState extends Equatable {}
+
+class InitialHomeState extends HomeState {
+  @override
+  List<Object?> get props => [runtimeType];
+}
+
+class LoadingHomeState extends HomeState {
+  @override
+  List<Object?> get props => [runtimeType];
+}
+
+class FailureHomeState extends HomeState {
+  @override
+  List<Object?> get props => [runtimeType];
+}
+
+class SuccessHomeState extends HomeState {
+  final List<PostEntity> posts;
+
+  SuccessHomeState({required this.posts});
+
+  @override
+  List<Object?> get props => [runtimeType];
+}
